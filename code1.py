9229cdfa498d3ab9d5caf167d52a6f025de727ac
JiadeLiu <2285969145@qq.com>
2020-01-13 15:27:33 +0800

    first commit

---
+++ code1.py
@@ -0,0 +1,109 @@
+from __future__ import print_function
+import torch
+
+# x = torch.empty(5, 3)
+# print(x)
+
+# y = torch.rand(5, 3)
+# print(y)
+
+# z = torch.zeros(5, 3, dtype=torch.long)
+# print(z)
+
+# x1 = torch.tensor([5.5, 3])
+# print(x1)
+#
+# x1 = x1.new_ones(5, 3, dtype=torch.double)
+# print(x1)
+#
+# x1 = torch.randn_like(x1, dtype=torch.float)
+# print(x1)
+
+# x2 = torch.rand(5, 3)
+# print(x2)
+#
+# x3 = torch.rand(5, 3)
+# print(x3)
+#
+# print(x2+x3)
+# print(torch.add(x2, x3))
+
+# x = torch.rand(5, 3)
+# print(x)
+#
+# print(x[:, 1])
+
+# x = torch.randn(4, 4)
+# y = x.view(16)
+# z = x.view(-1, 8)
+# print(x.size(), y.size(), z.size())
+
+# x = torch.randn(1)
+# print(x)
+# print(x.item())
+
+# a = torch.ones(5)
+# print(a)
+#
+# b = a.numpy()
+# print(b)
+
+# import numpy as np
+# a = np.ones(5)
+# b = torch.from_numpy(a)
+# np.add(a, 1, out=a)
+# print(a)
+# print(b)
+
+# x = torch.randn(5, 3)
+#
+# if torch.cuda.is_available():
+#    device = torch.device("cuda")
+#    y = torch.ones_like(x, device=device)
+#    x = x.to(device)
+#    z = x + y
+#    print(z)
+#    print(z.to("cpu", torch.double))
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
